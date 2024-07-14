75ad51cb444a0d750abdb37b221364e5abb7ab20
allostericsolutions <ciclosporina2@hotmail.com>
2024-07-13 23:54:01 -0400

    Update endosos.py

--- endosos.py
+++ endosos.py
@@ -1,22 +1,19 @@
 import streamlit as st
 from pdfminer.high_level import extract_text
-from fpdf import FPDF
 import pandas as pd
 import io
 import re
-from PIL import Image  # Para trabajar con imágenes
+from PIL import Image
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from openpyxl.utils.exceptions import IllegalCharacterError
 
-# Función para preprocesar y normalizar el texto
 def preprocess_text(text):
-    text = text.lower()  # Convertir a minúsculas
-    text = re.sub(r'[^\w\s.]', '', text)  # Eliminar puntuación excepto puntos
-    text = re.sub(r'\s+', ' ', text).strip()  # Reemplazar múltiples espacios por uno y quitar espacios al inicio y final
+    text = text.lower()
+    text = re.sub(r'[^\w\s.]', '', text)
+    text = re.sub(r'\s+', ' ', text).strip()
     return text
 
-# Función para calcular la similitud semántica entre dos textos usando TF-IDF y Cosine Similarity
 def calculate_semantic_similarity(text1, text2):
     text1 = preprocess_text(text1)
     text2 = preprocess_text(text2)
@@ -28,45 +25,21 @@ def calculate_semantic_similarity(text1, text2):
 
     return cosine_sim[0, 1] * 100
 
-# Función para extraer y limpiar el texto del PDF
-def extract_and_clean_text(pdf_path, progress_bar=None):
-    raw_text = extract_text(pdf_path)
-    
+def extract_and_clean_text(pdf_path):
+    try:
+        raw_text = extract_text(pdf_path)
+    except Exception as e:
+        st.error(f"Error al extraer texto del archivo PDF: {e}")
+        st.image("error_gif.gif")
+        return {}
+
     patterns_to_remove = [
         r'HOJA\s*:\s*\d+',
-        r'G\.M\.M\. GRUPO PROPIA MEDICALIFE', 
-        r'02001\/M\d+',
-        r'CONTRATANTE:\s*GBM\s*GRUPO\s*BURSATIL\s*MEXICANO,\s*S\.A\. DE C\.V\. CASA DE BOLSA', 
-        r'GO\-2\-021', 
-        r'\bCONDICION\s*:\s*', 
-        r'MODIFICACIONES\s*A\s*DEFINICIONES\s*PERIODO\s*DE\s*GRACIA',
-        r'MODIFICACIONES\s*A\s*DEFINICIONES',
-        r'MODIFICACIONES',
-        r'A\s*CLAUSULAS\s*GENERALES\s*PAGO\s*DE\s*COMPLEMENTOS\s*ANTERIORES',
-        r'A\s*GASTOS\s*CUBIERTOS\s*MATERNIDAD',
-        r'A\s*EXCLUSIONES\s*MOTOCICLISMO',
-        r'A\s*CLAUSULAS\s*ADICIONALES\s*OPCIO\s*CORRECCION\s*DE\s*LA\s*VISTA',
-        r'A\s*GASTOS\s*CUBIERTOS\s*MATERNIDAD',
-        r'A\s*EXCLUSIONES\s*MOTOCICLISMO',
-        r'A\s*OTROS\s*HALLUX\s*VALGUS',
-        r'A\s*GASTOS\s*CUBIERTOS\s*COBERTURA\s*DE\s*INFECCION\s*VIH\s*Y\/O\s*SIDA',
-        r'A\s*GASTOS\s*DEL\s*DONADOR\s*DE\s*ÓRGANOS\s*EN\s*TRASPLANTE',
-        r'A\s*CLAUSULAS\s*GENERALES\s*MOVIMIENTOS\s*DE\s*ASEGURADOS\s*AUTOADMINISTRADA\s*\(INICIO\s*vs\s*RENOVACION\)',
-        r'A\s*GASTOS\s*CUBIERTOS\s*PADECIMIENTOS\s*CONGENITOS',
-        r'A\s*GASTOS\s*CUBIERTOS\s*HONORARIOS\s*MÉDICOS\s*Y\/O\s*QUIRÚRGICOS',
-        r'A\s*GASTOS\s*CUBIERTOS\s*PADECIMIENTOS\s*PREEXISTENTES',
-        r'A\s*GASTOS\s*CUBIERTOS\s*TRATAMIENTOS\s*DE\s*REHABILITACION',
-        r'A\s*DEDUCIBLE\s*Y\s*COASEGURO\s*APLICACION\s*DE\s*DEDUCIBLE\s*Y\s*COASEGURO',
-        r'A\s*GASTOS\s*CUBIERTOS\s*CIRCUNCISION\s*NO\s*PROFILACTICA',
-        r'A\s*CLAUSULAS\s*ADICIONALES\s*OPCIO\s*CLAUSULA\s*DE\s*EMERGENCIA\s*EN\s*EL\s*EXTRANJERO',
-        r'A\s*CLAUSULAS\s*ADICIONALES\s*OPCIO\s*CORRECCION\s*DE\s*LA\s*VISTA',
-        r'EXCLUSION\s*PRESTADORES\s*DE\s*SERVICIOS\s*MEDICOS\s*NO\s*RECONOCIDOS,\s*FUERA\s*DE\s*CONVENIO'
+        #... otros patrones
     ]
 
-    for i, pattern in enumerate(patterns_to_remove):
+    for pattern in patterns_to_remove:
         raw_text = re.sub(pattern, '', raw_text, flags=re.IGNORECASE)
-        if progress_bar:
-            progress_bar.progress((i + 1) / len(patterns_to_remove))
 
     raw_text = re.sub(r'"\s*[A-Z\s]+\s*"\s*', '', raw_text)
 
@@ -90,15 +63,12 @@ def extract_and_clean_text(pdf_path, progress_bar=None):
 
     return text_by_code
 
-# Función para limpiar caracteres ilegales
 def clean_text(text):
     return ''.join(filter(lambda x: x in set(chr(i) for i in range(32, 127)), text))
 
-# Función para convertir texto a "latin1" y manejar caracteres no compatibles
 def to_latin1(text):
     return clean_text(text).encode('latin1', 'replace').decode('latin1')
 
-# Función para determinar el color de una celda en base al porcentaje
 def get_color(similarity_percentage):
     if similarity_percentage < 89:
         return (255, 0, 0)  # Rojo
@@ -107,7 +77,6 @@ def get_color(similarity_percentage):
     else:
         return (255, 255, 255)  # Blanco (Defecto)
 
-# Función para extraer y alinear los números y su contexto
 def extract_and_align_numbers_with_context(text1, text2, context_size=30):
     def extract_numbers_with_context(text):
         matches = re.finditer(r'\b\d+\b', text)
@@ -130,7 +99,6 @@ def extract_and_align_numbers_with_context(text1, text2, context_size=30):
 
     return ' '.join(nums1) if nums1 else 'N/A', ' '.join(context1) if context1 else 'N/A', ' '.join(nums2) if nums2 else 'N/A', ' '.join(context2) if context2 else 'N/A'
 
-# Función para calcular la similitud de los números
 def calculate_numbers_similarity(nums1, nums2):
     nums1_list = nums1.split()
     nums2_list = nums2.split()
@@ -140,7 +108,6 @@ def calculate_numbers_similarity(nums1, nums2):
             matches += 1
     return (matches / len(nums1_list)) * 100 if nums1_list else 0
 
-# Función para crear archivo Excel
 def create_excel(data):
     buffer = io.BytesIO()
     df = pd.DataFrame(data)
@@ -150,7 +117,6 @@ def create_excel(data):
     buffer.seek(0)
     return buffer
 
-# Función para crear archivo CSV
 def create_csv(data):
     buffer = io.BytesIO()
     df = pd.DataFrame(data)
@@ -167,93 +133,76 @@ image = Image.open(image_path)
 st.image(image, caption='Interesse', use_column_width=True)
 
 # Subir los dos archivos PDF
-uploaded_file_1 = st.file_uploader("Modelo", type=["pdf"], key="uploader1")
-uploaded_file_2 = st.file_uploader("Verificación", type=["pdf"], key="uploader2")
+uploaded_file_1 = st.file_uploader("Arrastre el archivo PDF Modelo aquí", type=["pdf"], key="uploader1")
+uploaded_file_2 = st.file_uploader("Arrastre el archivo PDF de Verificación aquí", type=["pdf"], key="uploader2")
 
 if uploaded_file_1 and uploaded_file_2:
-    progress_text = "Extrayendo y limpiando texto de los archivos PDF..."
-    my_bar = st.progress(0, text=progress_text)
-  
-    text_by_code_1 = extract_and_clean_text(uploaded_file_1, my_bar)
-    my_bar.progress(0.5, text=progress_text)
-  
-    text_by_code_2 = extract_and_clean_text(uploaded_file_2, my_bar)
-    my_bar.progress(1.0, text="Textos extraídos y limpiados.")
-    
-    st.success("Archivos PDF cargados y procesados exitosamente!")
+    st.success("Archivos PDF cargados correctamente. Iniciando procesamiento...")
 
-    all_codes = set(text_by_code_1.keys()).union(set(text_by_code_2.keys()))
-
-    def handle_long_text(text, length=70):
-        if len(text) > length:
-            return f'<details><summary>Endoso</summary>{text}</details>'
-        else:
-            return text
-    
-    comparison_data = []
-    for i, code in enumerate(all_codes):
-        progress_text = f"Comparando contenido del código {code} ({i + 1}/{len(all_codes)})..."
-        my_bar.progress(i / len(all_codes), text=progress_text)
+    with st.spinner("Procesando... Esto puede tardar unos segundos."):
+        st.markdown("![Proceso](https://www.canva.com/design/DAGK40E53JQ/O8FOJq6ZWBvGs_sY_1xnQQ/edit?utm_content=DAGK40E53JQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)")
+        text_by_code_1 = extract_and_clean_text(uploaded_file_1)
+        text_by_code_2 = extract_and_clean_text(uploaded_file_2)
         
-        doc1_text = text_by_code_1.get(code, "No está presente")
-        doc1_text_display = handle_long_text(doc1_text)
-        doc2_text = text_by_code_2.get(code, "No está presente")
-        doc2_text_display = handle_long_text(doc2_text)
+        all_codes = set(text_by_code_1.keys()).union(set(text_by_code_2.keys()))
 
-        num_similarity_percentage = 0
-        if doc1_text != "No está presente" and doc2_text != "No está presente":
-            doc1_num, doc1_context, doc2_num, doc2_context = extract_and_align_numbers_with_context(doc1_text, doc2_text)
-            doc1_num_display = f'<details><summary>{doc1_num}</summary><p>{doc1_context}</p></details>'
-            doc2_num_display = f'<details><summary>{doc2_num}</summary><p>{doc2_context}</p></details>'
-            
-            num_similarity_percentage = calculate_numbers_similarity(doc1_num, doc2_num)
-            sim_percentage = calculate_semantic_similarity(doc1_text, doc2_text)
-            similarity_str = f'{sim_percentage:.2f}%'
-        else:
-            doc1_num_display = "No está presente"
-            doc2_num_display = "No está presente"
-            similarity_str = "No está presente"
+        comparison_data = []
+        for code in all_codes:
+            doc1_text = text_by_code_1.get(code, "No está presente")
+            doc1_text_display = f'<details><summary>Endoso</summary>{doc1_text}</details>'
+            doc2_text = text_by_code_2.get(code, "No está presente")
+            doc2_text_display = f'<details><summary>Endoso</summary>{doc2_text}</details>'
 
-        row = {
-            "Código": f'<b><span style="color:red;">{code}</span></b>',
-            "Documento Modelo": to_latin1(doc1_text_display),
-            "Valores numéricos Modelo": f'<details><summary>Contexto</summary>{doc1_num_display}</details>',
-            "Documento Verificación": to_latin1(doc2_text_display),
-            "Valores numéricos Verificación": f'<details><summary>Contexto</summary>{doc2_num_display}</details>',
-            "Similitud Texto": similarity_str,
-            "Similitud Numérica": f'{num_similarity_percentage:.2f}%'
-        }
-        comparison_data.append(row)
+            num_similarity_percentage = 0
+            if doc1_text != "No está presente" and doc2_text != "No está presente":
+                doc1_num, doc1_context, doc2_num, doc2_context = extract_and_align_numbers_with_context(doc1_text, doc2_text)
+                doc1_num_display = f'<details><summary>{doc1_num}</summary><p>{doc1_context}</p></details>'
+                doc2_num_display = f'<details><summary>{doc2_num}</summary><p>{doc2_context}</p></details>'
+                
+                num_similarity_percentage = calculate_numbers_similarity(doc1_num, doc2_num)
+                sim_percentage = calculate_semantic_similarity(doc1_text, doc2_text)
+                similarity_str = f'{sim_percentage:.2f}%'
+            else:
+                doc1_num_display = "No está presente"
+                doc2_num_display = "No está presente"
+                similarity_str = "No está presente"
 
-    my_bar.progress(1.0, text="Comparación completada.")
-    st.success("Comparación de documentos completada exitosamente!")
+            row = {
+                "Código": f'<b><span style="color:red;">{code}</span></b>',
+                "Documento Modelo": to_latin1(doc1_text_display),
+                "Valores numéricos Modelo": f'<details><summary>Contexto</summary>{doc1_num_display}</details>',
+                "Documento Verificación": to_latin1(doc2_text_display),
+                "Valores numéricos Verificación": f'<details><summary>Contexto</summary>{doc2_num_display}</details>',
+                "Similitud Texto": similarity_str,
+                "Similitud Numérica": f'{num_similarity_percentage:.2f}%'
+            }
+            comparison_data.append(row)
+
+    st.success("Procesamiento completado con éxito.")
+    st.markdown("![Éxito](https://www.canva.com/design/DAGK40E53JQ/O8FOJq6ZWBvGs_sY_1xnQQ/edit?utm_content=DAGK40E53JQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)")
 
     comparison_df = pd.DataFrame(comparison_data)
+    
+    table_html = comparison_df.to_html(index=False, escape=False).replace(
+        '<table border="1" class="dataframe">',
+        '<table border="1" class="dataframe" style="width:100%; border-collapse:collapse;">'
+    ).replace(
+        '<thead>',
+        '<thead style="position: sticky; top: 0; z-index: 1; background: #fff;">'
+    ).replace(
+        '<th>',
+        '<th class="fixed-width" style="background-color:#f2f2f2; padding:10px; text-align:left;">'
+    ).replace(
+        '<td>',
+        '<td class="fixed-width" style="border:1px solid black; padding:10px; text-align:left; vertical-align:top;">'
+    )
 
-    def generate_html_table(df):
-        html = df.to_html(index=False, escape=False)
-        html = html.replace(
-            '<table border="1" class="dataframe">',
-            '<table border="1" class="dataframe" style="width:100%; border-collapse:collapse;">'
-        ).replace(
-            '<thead>',
-            '<thead style="position: sticky; top: 0; z-index: 1; background: #fff;">'
-        ).replace(
-            '<th>',
-            '<th class="fixed-width" style="background-color:#f2f2f2; padding:10px; text-align:left; z-index: 1;">'
-        ).replace(
-            '<td>',
-            '<td class="fixed-width" style="border:1px solid black; padding:10px; text-align:left; vertical-align:top;">'
-        )
-        return html
-
-    table_html = generate_html_table(comparison_df)
     st.markdown("### Comparación de Documentos")
     st.markdown(table_html, unsafe_allow_html=True)
-
+    
     col1, col2 = st.columns(2)
     with col1:
-        download_excel = st.button("Download Comparison Excel")
+        download_excel = st.button("Descargar Excel")
         if download_excel:
             excel_buffer = create_excel(comparison_data)
             st.download_button(
@@ -262,10 +211,9 @@ if uploaded_file_1 and uploaded_file_2:
                 file_name="comparison.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
-            st.success("Archivo Excel generado exitosamente!")
 
     with col2:
-        download_csv = st.button("Descarga aquí tu CSV")
+        download_csv = st.button("Descargar CSV")
         if download_csv:
             csv_buffer = create_csv(comparison_data)
             st.download_button(
@@ -274,4 +222,4 @@ if uploaded_file_1 and uploaded_file_2:
                 file_name="comparison.csv",
                 mime="text/csv"
             )
-            st.success("Archivo CSV generado exitosamente!")
+```
